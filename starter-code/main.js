a3a90213076f343ea1c24a8e70a7081ad29051e6
Aneela Ahmed <Aneela@Aneelas-MacBook-Air.local>
2016-10-13 00:30:19 -0400

    Cards using JS

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,13 +1,25 @@
-console.log("JS file is connected to HTML! Woo!")
+// console.log("JS file is connected to HTML! Woo!")
 
 
-var cardOne = "queen";
-var	cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+// var cardOne = "queen";
+// var	cardTwo = "queen";
+// var cardThree = "king";
+// var cardFour = "king";
 
-if (cardTwo === cardFour){
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again");
+// if (cardTwo === cardFour){
+// 	alert("You found a match!");
+// } else {
+// 	alert("Sorry, try again");
+// // }
+var number_of_cards = 4
+var board = document.getElementById('game-board');
+
+function createCards() {
+for (var i=0; i < number_of_cards; i++) { 
+	var cardElement = document.createElement('div');
+	cardElement.className = 'card';
+	board.appendChild(cardElement);
 }
+};
+
+createCards();
